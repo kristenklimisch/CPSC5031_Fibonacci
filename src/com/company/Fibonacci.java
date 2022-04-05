29bb919a69bfc8ac939bdce7b9091a0d0bb9d60c
kristenklimisch <kristen.klimisch@gmail.com>
2022-04-04 21:17:37 -0700

    Initial commit

---
+++ src/com/company/Fibonacci.java
@@ -0,0 +1,26 @@
+/*
+Kristen Klimisch
+CPSC-5031-02
+
+ */
+
+
+package com.company;
+
+/*
+Computes the nth term in the Fibonacci sequence using 3 different methods:
+classic recursive, iterative, and recursive with accumulators.
+Counts the number of addition operations used by each technique to
+compute the nth term in the Fibonacci sequence.
+ */
+public class Fibonacci {
+    private int n;
+
+    Fibonacci() {
+        n = 1;
+    }
+
+    public int getN() {
+        return n;
+    }
+}
